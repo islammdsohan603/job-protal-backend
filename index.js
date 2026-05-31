8614ef4229c5f439f0f12e77d91ce25341979310
islammdsohan603 <islammdsohan603@gmail.com>
2026-05-31 09:05:17 +0600

    all jobs data

--- index.js
+++ index.js
@@ -11,6 +11,40 @@ app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
+const uri = process.env.MONGO_DB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+
+    const db = client.db("jobprotal");
+    const jobs = db.collection("jobs");
+
+    app.get("/jobs", async (req, res) => {
+      const data = jobs.find();
+      const result = await data.toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } catch (error) {
+    console.log(`Error now ${error}`);
+  }
+};
+
+run();
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
